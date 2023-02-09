064d8e3745d0ad844a01b146271b49209c13a36c
Pelle Bloch Atkinson <stillconnecting@gmail.com>
2023-02-09 11:49:43 +0100

    Fixed Compiler issues

--- Assets/Samples/Modularis/0.0.4/Custom Variable Example/PlayerReference.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Devkit.Modularis.References;
-using OdinSerializer;
-
-namespace Devkit.Modularis.Examples
-{
-    [Serializable]
-    public class PlayerReference : BaseReference<PlayerVariable,Player> { }
-}
