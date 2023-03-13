f25e6fee48032ebae4129f5b538a222207315d92
Kit <kval15@student.aau.dk>
2023-03-13 12:10:37 +0100

    push til pelle

--- Assets/Scripts/Gesture.cs
+++ Assets/Scripts/Gesture.cs
@@ -181,7 +181,18 @@ public class Gesture
         Z,
         Æ,
         Ø,
-        Å
+        Å,
+        N0,
+        N1,
+        N2,
+        N3,
+        N4,
+        N5,
+        N6,
+        N7,
+        N8,
+        N9,
+        N10
     }
 
 
